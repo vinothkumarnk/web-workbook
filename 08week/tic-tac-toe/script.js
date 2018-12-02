0eabeb4b5455e06ca0ec514a64a311422cb82b7e
Vinoth Kumar Nanda Kumar <vinothkumar.nk@gmail.com>
2018-12-02 07:50:32 -0600

    Dom-practice and Tic-tac

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,35 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic in here
+$(document).ready(function () {
+  var turn = 'X';
+  $('[data-cell]').on('click', function () {
+    if ($(this).text() === '') {
+      $(this).text(turn);
+      checkForWin();
+      if (turn === 'O') {
+        turn = 'X';
+      } else {
+        turn = 'O';
+      }
+    }
+  })
+
+  // if theres a win on row 1...
+  function checkForWin() {
+    if ($('[data-cell="0"]').text() === turn &&
+      $('[data-cell="1"]').text() === turn &&
+      $('[data-cell="2"]').text() === turn) {
+      $('#announce-winner').text(`${turn} wins`);
+    }
+    if ($('[data-cell="3"]').text() === turn &&
+      $('[data-cell="4"]').text() === turn &&
+      $('[data-cell="5"]').text() === turn) {
+      $('#announce-winner').text(`${turn} wins`);
+    }
+    if ($('[data-cell="6"]').text() === turn &&
+      $('[data-cell="7"]').text() === turn &&
+      $('[data-cell="8"]').text() === turn) {
+      $('#announce-winner').text(`${turn} wins`);
+    }
+  }
 });
